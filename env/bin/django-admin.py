cc8be5f89f6de20a21f6536e58c104ee840d5104
antialkash90210 <alek.kalashnikov32@yandex.ru>
2025-06-17 10:05:43 +0000

    after

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/kalash/test/DjangoApp-Nginx_PostgreSQL_PgAdmin_Docker/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
